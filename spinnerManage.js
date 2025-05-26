db0bbf8816020a13865f5f1674249afc8829af2f
AlexWeinreb <alexis.weinreb@yale.edu>
2025-05-26 16:04:50 -0400

    remove spinnerManage.js
    doesn't seem used anywhere in current app

--- spinnerManage.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-            $('#klickButton').click(function() {
-            $(".loading-spinner").show();
-        });  
-    });
-    $(document).on("shiny:connected", function(e) {
-            $(".loading-spinner").hide();
-    });
