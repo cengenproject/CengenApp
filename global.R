7cbc0fc9e69938b61a206465ac69a7638120c5b5
AlexWeinreb <alexis.weinreb@yale.edu>
2025-06-05 19:43:23 -0400

    Reorder global.R

--- global.R
+++ global.R
@@ -1,59 +1,6 @@
 
-# read config file
-config <- list.files("deployment/", pattern = "config_", full.names = TRUE)
-
-if(length(config) == 1){
-  source(config)
-} else if(length(config) > 1){
-  
-  # here set default file
-  source("deployment/config_adult.R")
-  
-} else{
-  stop("Configuration file not found")
-}
-
-
-
-
-message("-------------  dataset: ", dataset," ----------------")
-
-# check config loaded
-stopifnot(nchar(dataset) > 1)
-stopifnot(file.exists(file.path("www", favicon) ))
-stopifnot(file.exists(file.path("www", icon_big) ))
-stopifnot(dir.exists(data_dir))
-stopifnot( length(list.files(data_dir)) > 5 )
-stopifnot(nchar(bg_color) > 4)
-
-
-source("R/load_as_needed.R")
-source("R/perform_de.R")
-source("R/plot_heatmap.R")
-
-# variables directly defined from config file
-
-favicon <- paste0(dataset, ".png")
-icon_big <- paste0(dataset, "_big.png")
-
-
-available_apps <- data.frame(
-  name = c("L1", "L4", "adult", "male"),
-  url = c("L1app", "L4app", "adult", "male")
-)
-
-other_apps <- available_apps[available_apps$name != dataset,]
-
-
-footer <- div(
-  h5("Citation:"),
-  paper_authors, br(),
-  em(paper_title),
-  paper_date_venue, br(),
-  paper_footnote,
-  class = "alert alert-info mt-5"
-)
-
+# if there are several configuration files available, which one to use as default
+default_config <- "deployment/config_adult.R"
 
 
 # shared variables for all apps
@@ -80,7 +27,43 @@ male_neurons <- c("CA1-4", "CA5-6", "CA7", "CA8-9", "CEM", "CP0", "CP1-4", "CP5-
 
 
 
-# load global data
+
+#--- no need to update below ---
+
+
+# read config file
+config <- list.files("deployment/", pattern = "config_", full.names = TRUE)
+
+if(length(config) == 1){
+  
+  source(config)
+} else if(length(config) > 1){
+  
+  source(default_config)
+} else{
+  
+  stop("Configuration file not found")
+}
+rm(config)
+
+
+
+message("-------------  dataset: ", dataset," ----------------")
+
+# check config loaded
+stopifnot(nchar(dataset) > 1)
+stopifnot(file.exists(file.path("www", favicon) ))
+stopifnot(file.exists(file.path("www", icon_big) ))
+stopifnot(dir.exists(data_dir))
+stopifnot( length(list.files(data_dir)) > 5 )
+stopifnot(nchar(bg_color) > 4)
+
+# load global data and functions
+
+source("R/load_as_needed.R")
+source("R/perform_de.R")
+source("R/plot_heatmap.R")
+
 load_as_needed("gene_list")
 load_as_needed("all_cell_types")
 load_as_needed("all_neuron_types")
@@ -89,6 +72,33 @@ if(dataset == "male"){
   male_all_cell_types <- qs::qread(file.path(data_dir, "all_cell_types.qs"))
   herm_all_cell_types <- qs::qread(herm_all_cell_types_path)
 }
+
+
+
+# variables directly defined from config file
+
+favicon <- paste0(dataset, ".png")
+icon_big <- paste0(dataset, "_big.png")
+
+
+available_apps <- data.frame(
+  name = c("L1", "L4", "adult", "male"),
+  url = c("L1app", "L4app", "adult", "male")
+)
+
+other_apps <- available_apps[available_apps$name != dataset,]
+
+
+footer <- div(
+  h5("Citation:"),
+  paper_authors, br(),
+  em(paper_title),
+  paper_date_venue, br(),
+  paper_footnote,
+  class = "alert alert-info mt-5"
+)
+
+
 
 
 
